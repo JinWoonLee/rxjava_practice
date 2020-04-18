de2e62c1729da217f77932e8539b1ec62b2c99c4
jilee01 <jilee01@coupang.com>
2020-04-18 20:44:38 +0900

    set project

--- src/main/java/rxjava/lambda/LambdaPractice.java
+++ src/main/java/rxjava/lambda/LambdaPractice.java
@@ -11,17 +11,5 @@ public class LambdaPractice {
     }
 
     public static void main(String[] args) {
-        String[][] strs = {{"A", "1"}, {"B", "2"}};
-        Map<String, String> stringMap = Stream.of(strs)
-                .collect(Collectors.toMap((str -> str[0]), (str -> str[1]), (a, b) -> a+b));
-
-        stringMap.forEach((key, value) -> System.out.println(key + " " + value));
-
-
-        System.out.println(EnumTest.valueOf("F1"));
-
-
-        String[] str = {"1", "2"};
-
     }
 }
