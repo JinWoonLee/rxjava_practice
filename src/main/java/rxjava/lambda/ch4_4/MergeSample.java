adfeb15141cc6d70de70c4cb861bf165a9cd4ded
mouseeye <mouseeye@naver.com>
2020-04-26 20:38:37 +0900

    ch4 temp

--- src/main/java/rxjava/lambda/ch4_4/MergeSample.java
+++ src/main/java/rxjava/lambda/ch4_4/MergeSample.java
@@ -1,4 +1,23 @@
 package rxjava.lambda.ch4_4;
 
+import io.reactivex.Flowable;
+import rxjava.lambda.ch4.DebugSubscriber;
+
+import java.util.concurrent.TimeUnit;
+
 public class MergeSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Long> flowable1 = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .take(5);
+
+        Flowable<Long> flowable2 = Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                .take(2)
+                .map(data -> data + 100);
+
+        Flowable.merge(flowable1, flowable2)
+                .subscribe(new DebugSubscriber<>("merge"));
+
+        Thread.sleep(5000);
+    }
 }
